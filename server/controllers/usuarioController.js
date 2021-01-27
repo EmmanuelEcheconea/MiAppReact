4df98822fc9b28c961633357054a4cd47618c13e
Emmanuel <eecheconea@gmail.com>
2021-01-26 23:18:32 -0300

    bcrypt Hash contrasena, ver que no se repita Email

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -1,15 +1,30 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 exports.crearUsuario = async (req,res) => {
+
+    const {email, contrasena} = req.body;
+
+
     try{
 
-        let usuario;
+        //verifico que no haya otro usuario igual
+        let usuario = await Usuario.findOne({email});
+        if( usuario)
+        {
+            return res.status(400).json({msg: 'el usuario ya existe'});
+        }
+
         //crear el usuario a partir de lo q viene del ladod el front
         usuario = new Usuario(req.body);
-
+        
+        //hasheo el pass
+        const salt = await bcryptjs.genSalt(10);
+        usuario.contrasena = await bcryptjs.hash(contrasena , salt);
+        
         //guarda ese usuario
         await usuario.save();
 
-        res.send("usuario guardado");
+        res.json({msg: 'Usuario Creado Correctamente'});
     }catch(err){
         console.log(err);
         res.status(400).send("hubo un error");
